b1a035da81824560366b6b81370f3d7dede118c8
trevor smith <trevorsmithbanjo@gmail.com>
2021-09-23 12:25:19 -0500

    adds netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,7 @@ module.exports = {
       }
     },
     'gatsby-transformer-remark',
-    'gatsby-plugin-styled-components'
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-netlify'
   ],
 }
